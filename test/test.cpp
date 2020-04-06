59b6491b7bc9a677625d1a31b3ee2e4d19038fdb
root <root@localhost.localdomain>
2020-04-07 01:27:02 +0800

    rc2

--- test/test.cpp
+++ test/test.cpp
@@ -16,7 +16,26 @@ int main(int argc, char *argv[])
     if(ini==NULL)
         DIE("ini create failed, errno_type=%d, errno=%d", ini_errno_type(), ini_errno());
     INF("ini create success");
+    INF("nothing=%lu",ini_section_count(ini,""));
+    INF("hello=%lu",ini_section_count(ini,"hello"));
     ret=ini_find(ini,0,"hello","name",&value);
+    if(ret!=0)
+        INF("don't find");
+    else
+        INF("value=%s", value);
+    ret=ini_find(ini,0,"","jack",&value);
+    if(ret!=0)
+        INF("don't find");
+    else
+        INF("value=%s", value);
+
+    ret=ini_find(ini,1,"hello","name",&value);
+    if(ret!=0)
+        INF("don't find");
+    else
+        INF("value=%s", value);
+
+    ret=ini_find(ini,0,"yang","name",&value);
     if(ret!=0)
         INF("don't find");
     else
